43bb67242cbf842c73eda66b2a0cfa6314c93e8e
yohanse <mehabawyohanse793@gmail.com>
2025-03-07 22:41:58 +0300

    create urls

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,16 @@ from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 
-router.register("products", views.ProductView, basename='products')
+router.register("products", views.ProductViewSet, basename='products')
+router.register("catagorys", views.CatagoryViewSet, basename='catagorys')
+router.register("customers", views.CustomerViewSet, basename='customers')
+router.register("carts", views.CartViewSet, basename='carts')
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router, "products", lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+cart_item = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_item.register('items', views.CartItemViewSet, basename='cart-items')
+
+
+urlpatterns = router.urls + cart_item.urls + products_router.urls
